8ac5359c9ae58575b360d820156dba99f7fadab6
Copy <copyin2003@gmail.com>
2017-03-25 03:58:14 +0800

    [AndroidTools]init AndroidTools Project

---
+++ AndroidTools/src/com/cc/android/tools/Const.java
@@ -0,0 +1,15 @@
+package com.cc.android.tools;
+
+class Const {
+    protected static final String NO_DEVICE = "没有设备";
+    protected static final String FALSH_DEVICE = "刷新设备";
+    protected static final String FEATURE_LIST = "功能列表";
+    protected static final String DEVICE_MANAGE = "设备管理";
+    protected static final String APP_MANAGE = "手机应用管理";
+    protected static final String FILE_MANAGE = "手机文件管理";
+    protected static final String APK_MANAGE = "apk管理";
+    protected static final String LOG_MANAGE = "日志管理";
+    protected static final String SETTINGS = "设置";
+    protected static final String ROOT_PERMISSION = "Root权限";
+    protected static final String ABOUT = "关于";
+}
